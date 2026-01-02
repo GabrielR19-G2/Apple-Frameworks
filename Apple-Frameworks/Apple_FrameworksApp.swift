c27785619ca5167e397f1db74f205a0863585f7d
gabriel ramos <gabrielfcop8@gmail.com>
2026-01-01 17:18:12 -0700

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Gabriel Ramos on 01/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
